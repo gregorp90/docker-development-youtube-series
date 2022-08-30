c27095cabea3826ed5284a216f33f20f76a5f70d
gregorp90 <gregor.pirs1@gmail.com>
2022-08-30 09:34:12 +0200

    Add python app logging

--- monitoring/logging/fluentd/introduction/python/app.py
+++ monitoring/logging/fluentd/introduction/python/app.py
@@ -1,24 +1,37 @@
-import logging
+# import logging
 import time
-
-from fluent import handler as fluent_handler, sender
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-h = fluent_handler.FluentHandler(
-    "app",
-    host="localhost",
-    port=int(24224),
-    nanosecond_precision=True,
-)
-
-logger.addHandler(h)
-
-log_handler = logging.StreamHandler()
-logger.addHandler(log_handler)
+#
+# from fluent import handler as fluent_handler, sender
+#
+# logger = logging.getLogger()
+# logger.setLevel(logging.INFO)
+#
+# h = fluent_handler.FluentHandler(
+#     "fluentd.test",
+#     host="localhost",
+#     port=int(24224),
+#     nanosecond_precision=True,
+# )
+#
+# logger.addHandler(h)
+#
+# log_handler = logging.StreamHandler()
+# logger.addHandler(log_handler)
+#
+# while True:
+#     logger.info("Send log")
+#     time.sleep(5)
+from fluent import sender
+from fluent import event
+sender.setup('fluentd.test', host='fluentd', port=24224)
+event.Event('follow', {
+  'from': 'userA',
+  'to':   'userB'
+})
 
 while True:
-    logger.info("Send log")
+    event.Event('follow', {
+        'from': 'userA',
+        'to': 'userB'
+    })
     time.sleep(5)
-
